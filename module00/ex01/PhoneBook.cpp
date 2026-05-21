e13bb9e31c71ee8b10ced26ac307c3f149bf503a
sashalinux <sasha.daniel2006@gmail.com>
2026-05-21 17:01:25 +0200

    feat: v1 PhoneBook::addUser

--- module00/ex01/PhoneBook.cpp
+++ module00/ex01/PhoneBook.cpp
@@ -1,7 +1,8 @@
 #include "PhoneBook.hpp"
 
-std::string	PhoneBook::prompt_menu(void)
-{
+typedef void (Contact::*contactMethod)(std::string);
+
+std::string	PhoneBook::prompt_menu(void) {
 	std::string cmd;
 
 	std::cout << "╔══════════════════════╗\n";
@@ -16,4 +17,29 @@ std::string	PhoneBook::prompt_menu(void)
 	std::cout << "\nEnter command: ";
 	getline(std::cin, cmd);
 	return (cmd);
+}
+
+void PhoneBook::addUser(void) {
+	std::string answer;
+	std::string prompts[5] = {
+		"Enter new contact's first name: ",
+		"Enter new contact's last name: ",
+		"Enter new contact's nickname: ",
+		"Enter new contact's phone number: ",
+		"Enter new contact's darkest secret: "
+	};
+	contactMethod methods[5] = {
+		&Contact::SetFirstName,
+		&Contact::SetLastName,
+		&Contact::SetNickname,
+		&Contact::SetPhoneNumber,
+		&Contact::SetDarkestSecret
+	};
+	Contact newUser;
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << prompts[i];
+		getline(std::cin, answer);
+		(newUser.*methods[i])(answer);
+	}
 }

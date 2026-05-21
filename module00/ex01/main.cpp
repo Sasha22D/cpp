a6082b936b52453ecde899f773b0b95d59893cab
sashalinux <sasha.daniel2006@gmail.com>
2026-05-21 19:13:17 +0200

    chore: loop main

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -7,8 +7,11 @@ int	main(int ac, char **av)
 	std::string cmd;
 	PhoneBook pb;
 
-	cmd = pb.prompt_menu();
-	if (cmd.compare("ADD") == 0) {
-		pb.addUser();
+	while (true)
+	{
+		cmd = pb.prompt_menu();
+		if (cmd.compare("ADD") == 0) {
+			pb.addUser();
+		}
 	}
 }

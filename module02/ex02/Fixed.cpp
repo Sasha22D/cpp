b3be2ec4263a38e56827be7d5954baff0f0185df
sadaniel <sadaniel@f4r2s6.paris.42.school>
2026-07-01 17:03:40 +0200

    v1 cpp02 ex02

---
+++ module02/ex02/Fixed.cpp
@@ -0,0 +1,79 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(): _fixed_point(0) {
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed( const int value ) {
+	std::cout << "Int ctor\n";
+	_fixed_point = value << _fractional_bits;
+}
+
+Fixed::Fixed( const float value ) {
+	std::cout << "Float ctor\n";
+	_fixed_point = roundf(value * (1 << _fractional_bits));
+}
+
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called\n";
+	_fixed_point = other.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called\n";
+	if (this != &other) {
+		_fixed_point = other.getRawBits();
+	}
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called\n";
+	return _fixed_point;
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called\n";
+	_fixed_point = raw;
+}
+
+float Fixed::toFloat() const {
+	return static_cast<float>(_fixed_point) / (1 << _fractional_bits);
+}
+
+int Fixed::toInt() const {
+	return _fixed_point >> _fractional_bits;
+}
+
+std::ostream& operator<<(std::ostream &out, const Fixed &point) {
+	out << point.toFloat();
+	return out;
+}
+
+int operator>( const Fixed& left, const Fixed& right ) {
+	return left._fixed_point > right._fixed_point;
+}
+
+int operator<( const Fixed& left, const Fixed& right ) {
+	return left._fixed_point < right._fixed_point;
+}
+
+int operator>=( const Fixed& left, const Fixed& right ) {
+	return left._fixed_point >= right._fixed_point;
+}
+
+int operator<=( const Fixed& left, const Fixed& right ) {
+	return left._fixed_point <= right._fixed_point;
+}
+
+int operator==( const Fixed& left, const Fixed& right ) {
+	return left._fixed_point == right._fixed_point;
+}
+
+int operator!=( const Fixed& left, const Fixed& right ) {
+	return left._fixed_point != right._fixed_point;
+}
---
+++ module02/ex02/Fixed.hpp
@@ -0,0 +1,50 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int	_fixed_point;
+	static const int _fractional_bits = 8;
+public:
+	Fixed( void );
+	Fixed( const int value );
+	Fixed( const float value );
+	Fixed(const Fixed& other);
+	Fixed& operator=( const Fixed& other );
+	~Fixed( void );
+	int	getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	friend int operator>( const Fixed& left, const Fixed& right );
+	friend int operator<( const Fixed& left, const Fixed& right );
+	friend int operator>=( const Fixed& left, const Fixed& right );
+	friend int operator<=( const Fixed& left, const Fixed& right );
+	friend int operator==( const Fixed& left, const Fixed& right );
+	friend int operator!=( const Fixed& left, const Fixed& right );
+	// friend int operator+( const Fixed& left, const Fixed& right );
+	// friend int operator-( const Fixed& left, const Fixed& right );
+	// friend int operator*( const Fixed& left, const Fixed& right );
+	// friend int operator/( const Fixed& left, const Fixed& right );
+};
+
+int operator>( const Fixed& left, const Fixed& right );
+int operator<( const Fixed& left, const Fixed& right );
+int operator>=( const Fixed& left, const Fixed& right );
+int operator<=( const Fixed& left, const Fixed& right );
+int operator==( const Fixed& left, const Fixed& right );
+int operator!=( const Fixed& left, const Fixed& right );
+
+// int operator+( const Fixed& left, const Fixed& right );
+// int operator-( const Fixed& left, const Fixed& right );
+// int operator*( const Fixed& left, const Fixed& right );
+// int operator/( const Fixed& left, const Fixed& right );
+
+std::ostream& operator<<(std::ostream &out, const Fixed &point);
+
+
+#endif

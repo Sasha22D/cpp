9be1106f95277136ce835018b9584e1292508361
sadaniel <sadaniel@f4r2s9.paris.42.school>
2026-07-03 13:47:29 +0200

    cpp03 check for hp and energy before doing actions

--- Module03/ex00/ClapTrap.cpp
+++ Module03/ex00/ClapTrap.cpp
@@ -9,7 +9,16 @@ ClapTrap::~ClapTrap() {
 }
 
 void ClapTrap::attack( const std::string &target) {
+    if (_hitPoints == 0) {
+        std::cout << "ClapTrap " << _name << " can\'t attack since it has no HP\n";
+        return ;
+    }
+    if (_energyPoints == 0) {
+        std::cout << "ClapTrap " << _name << " can\'t attack since it has no energy\n";
+        return ;
+    }
     std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!\n";
+    _energyPoints--;
 }
 
 void ClapTrap::takeDamage( unsigned int amount ) {
@@ -19,6 +28,14 @@ void ClapTrap::takeDamage( unsigned int amount ) {
 }
 
 void ClapTrap::beRepaired( unsigned int amount ) {
+    if (_hitPoints == 0) {
+        std::cout << "ClapTrap " << _name << " can\'t be repaired since it has no HP\n";
+        return ;
+    }
+    if (_energyPoints == 0) {
+        std::cout << "ClapTrap " << _name << " can\'t be repaired since it has no energy\n";
+        return ;
+    }
     _hitPoints += amount;
     std::cout << "ClapTrap " << _name << " gained " << amount << " hitpoints\n";
     std::cout << "ClapTrap " << _name << " has now " << _hitPoints << " hitpoints\n";

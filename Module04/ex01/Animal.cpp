261e7a34da244e951b5028263ca229f7d8e082a3
SashaWindows <sasha.daniel2006@gmail.com>
2026-07-08 15:52:51 +0200

    cpp04 ex01 Animal rule of three

--- Module04/ex01/Animal.cpp
+++ Module04/ex01/Animal.cpp
@@ -1,17 +1,30 @@
 #include "Animal.hpp"
 
 Animal::Animal() {
-    std::cout << "Animal ctor\n";
+	std::cout << "Animal ctor\n";
+}
+
+Animal::Animal( const Animal& other ) {
+	std::cout << "Animal copy ctor\n";
+	_type = other._type;
+}
+
+Animal& Animal::operator=( const Animal& other ) {
+	std::cout << "Animal copy assignment ctor\n";
+	if (this != &other) {
+		_type = other._type;
+	}
+	return *this;
 }
 
 Animal::~Animal() {
-    std::cout << "Animal dtor\n";
+	std::cout << "Animal dtor\n";
 }
 
 void Animal::makeSound() {
-    std::cout << "Animal makes sound\n";
+	std::cout << "Animal makes sound\n";
 }
 
 const std::string& Animal::getType() const {
-    return _type;
+	return _type;
 }

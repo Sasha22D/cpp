777cc1e9b6002eba977bf21f91a10d32a21b02cf
SashaWindows <sasha.daniel2006@gmail.com>
2026-07-08 15:53:09 +0200

    cpp04 ex01 Brain rule of three

--- Module04/ex01/Brain.hpp
+++ Module04/ex01/Brain.hpp
@@ -7,9 +7,11 @@
 class Brain
 {
 private:
-    std::string ideas[100];
+    std::string _ideas[100];
 public:
     Brain();
+    Brain( const Brain& other );
+    Brain& operator=( const Brain& other );
     ~Brain();
 };
 
